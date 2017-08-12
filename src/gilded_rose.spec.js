efde565d46cfe5ec4c576033646c417417b664a8
Stacey Vetzal <stacey@vetzal.com>
2017-08-12 08:47:47 -0400

    initial root

---
+++ src/gilded_rose.spec.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+import GuildedRose from './guilded_rose';
+
+describe('GuildedRose', () => {
+  // What on earth does this thing do??!!
+});
---
+++ src/guilded_rose.js
@@ -0,0 +1,51 @@
+export default class GuildedRose {
+
+  constructor(name, quality, days_remaining) {
+    this.name = name;
+    this.quality = quality;
+    this.days_remaining = days_remaining;
+  }
+
+  tick() {
+    if (this.name !== 'Aged Brie' && this.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.quality > 0) {
+        if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+          this.quality = this.quality - 1;
+        }
+      } else if (this.quality < 50) {
+        this.quality = this.quality + 1;
+        if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.sell_in < 11) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1;
+            }
+          }
+
+          if (this.sell_in < 6) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1;
+            }
+          }
+        }
+      }
+    }
+    if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+      this.sell_in = this.sell_in - 1;
+    }
+    if (this.sell_in < 0) {
+      if (this.name !== 'Aged Brie') {
+        if (this.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.quality > 0) {
+            if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+              this.quality = this.quality - 1;
+            }
+          }
+        } else {
+          this.quality = this.quality - this.quality;
+        }
+      } else if (this.quality < 50) {
+        this.quality = this.quality + 1;
+      }
+    }
+  }
+}
