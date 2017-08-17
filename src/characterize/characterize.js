c5d9a8483e7f4b59fe0411d36d05183912ed7e1f
Stacey Vetzal <stacey@vetzal.com>
2017-08-17 19:46:58 -0400

    cleaning up characterizer

--- src/characterize/characterize.js
+++ src/characterize/characterize.js
@@ -3,23 +3,23 @@ import GildedRose from '../gilded_rose';
 
 const tolerances = [ -2, -1, 0, 1, 2 ];
 
-function report(comment, g) {
-  console.log(comment, g.name, g.quality, g.days_remaining);
-}
+let learned = [];
 
 names.forEach(name => {
   qualityBoundaries.forEach(quality => {
     tolerances.forEach(quality_tolerance => {
-      sellInBoundaries.forEach(sell_in => {
-        tolerances.forEach(sell_in_tolerance => {
-          let g = new GildedRose(name, quality_tolerance, sell_in_tolerance);
+      sellInBoundaries.forEach(days_remaining => {
+        tolerances.forEach(days_tolerance => {
+          let record = {};
+          let g = new GildedRose(name, quality + quality_tolerance, days_remaining + days_tolerance);
+          record.input = [g.name, g.quality, g.days_remaining];
           g.tick();
-          if (g.quality !== quality_tolerance || g.days_remaining !== sell_in_tolerance) {
-            // report(`Interesting ${quality_tolerance} ${sell_in_tolerance}`, g);
-            console.log(`["${name}", [ ${quality_tolerance}, ${sell_in_tolerance} ], [ ${g.quality}, ${g.days_remaining} ]],`);
-          }
+          record.output = [g.name, g.quality, g.days_remaining];
+          learned.push(record);
         });
       });
     });
   });
 });
+
+console.log(JSON.stringify(learned, null, 2));
